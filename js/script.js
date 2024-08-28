888066398b84845358c50a60b30edf94937c8260
Jéssica Campos <jessica.aulasfr@gmail.com>
2024-08-28 19:31:51 -0300

    Finalizei os Exercícios.

--- js/script.js
+++ js/script.js
@@ -2,60 +2,90 @@
 
 
 function exibir() {
-    var nome = document.getElementById("nome").value
-    alert(`O seu nome completo é ${nome}.`)
+    const nome = document.getElementById("nome").value;
+    alert(`O seu nome completo é ${nome}.`);
 }
 
 
 // Crie uma variável para armazenar se você gosta de programar (true ou false)
 
-const sim = true;
-const nao = false;
-console.log(sim);
-console.log(nao);
+
+function verificarResposta() {
+  const resposta = document.getElementById("gostaDeProgramar").value.toUpperCase();
+  const resultado = document.getElementById("resultado");
+
+  if (resposta === "SIM") {
+    gostaDeProgramar = true; 
+        alert(`${gostaDeProgramar}, você gosta de programar!`);
+  } else if (resposta === "NAO") {
+    gostaDeProgramar = false; 
+    alert(`${gostaDeProgramar}, você não gosta de programar!`);
+  } else {
+    resultado.textContent = "Por favor, digite SIM ou NAO.";
+  }
+}
 
 
 // Dadas as operações matemáticas abaixo, calcule-as utilizando Javascript, para cada número utilizar uma variável
 
-//2 + (3 x 4) - 5
+function expressao1() {
+    const expressao1 = document.getElementById("expressao1").value;
+    const resultado1 = 2+(3*4)-5;
+    alert(`O resultado da expressão é ${resultado1}.`);
+}
 
-console.log(2+(3*4)-5);
+function expressao2() {
+    const expressao2 = document.getElementById("expressao2").value;
+    const resultado2 = (2*(3+4) -5 / 5);
+    alert(`O resultado da expressão é ${resultado2}.`);
+}
 
-//2 x (3 + 4) - 5 ÷ 5
-console.log(2*(3+4) -5 / 5);
+function expressao3() {
+    const expressao3 = document.getElementById("expressao3").value;
+    const resultado3 = ((((10-4)/2)*3)+1);
+    alert(`O resultado da expressão é ${resultado3}.`);
+}
 
-//{[(10 - 4) / 2] * 3} + 1
-console.log((((10-4)/2)*3)+1);
+function expressao4() {
+    const expressao4 = document.getElementById("expressao4").value;
+    const resultado4 = (((3*4)/2)+1-5);
+    alert(`O resultado da expressão é ${resultado4}.`);
+}
 
-//3 * 4 / 2 + 1 - 5
-console.log(((3*4)/2)+1-5);
 
 //Crie um poema utilizando interpolação para inserir variáveis em diferentes linhas
 
-var tema = "casa";
-var descricao = "muito engraçada";
-var teto = "não tinha teto";
-var nada = "não tinha nada";
-var parede = "não tinha parede";
-var pinico = "pinico não tinha ali";
-var esmero = "era feito com muito esmero";
-var rua = "rua dos Bobos";
-var numero = "número zero";
+const tema = "casa";
+const descricao = "muito engraçada";
+const teto = "Não tinha teto";
+const nada = "não tinha nada";
+const parede = "não tinha parede";
+const pinico = "pinico não tinha ali";
+const esmero = "era feito com muito esmero";
+const rua = "rua dos Bobos";
+const numero = "número zero";
 
 
-console.log(`
-    A Casa - Vinícius de Moraes
-Era uma ${tema}
-${descricao}
-${teto}
-${nada}
-Ninguém podia entrar nela, não
-Porque na casa ${parede}
-Ninguém podia fazer pipi
-Porque ${pinico}
-Mas ${esmero}
-Na ${rua}
-${numero}`);
+const poema = "A Casa - Vinícius de Moraes"
+const poema2 = `Era uma ${tema} ${descricao}`
+const poema3 = `${teto}, ${nada}`
+const poema4 = "Ninguém podia entrar nela, não"
+const poema5 = `Porque na casa ${parede}`
+const poema6 = "Ninguém podia fazer pipi"
+const poema7 = `Porque ${pinico}`
+const poema8 = `Mas ${esmero}`
+const poema9 = `Na ${rua},
+${numero}`;
+
+document.getElementById("poema").innerHTML = poema;
+document.getElementById("poema2").innerHTML = poema2;
+document.getElementById("poema3").innerHTML = poema3;
+document.getElementById("poema4").innerHTML = poema4;
+document.getElementById("poema5").innerHTML = poema5;
+document.getElementById("poema6").innerHTML = poema6;
+document.getElementById("poema7").innerHTML = poema7;
+document.getElementById("poema8").innerHTML = poema8;
+document.getElementById("poema9").innerHTML = poema9;
 
 
 
@@ -73,35 +103,38 @@ const dia = String(data.getDate()).padStart(2, '0');
 const mes = String(data.getMonth() + 1).padStart(2,'0');
 const ano = data.getFullYear();
 const dataAtual = `${dia}/${mes}/${ano}`;
-console.log(dataAtual);
 
+document.getElementById("dataAtual").innerHTML = dataAtual;
 
 
 //Crie um programa que calcule a área de um retângulo, solicitando a base e a altura ao usuário. Exiba o resultado utilizando interpolação
 
-var base= Number (window.prompt ('Digite em centímetros o valor da base do retângulo:'))
-var altura = Number(window.prompt ('Digite em centímetros o valor da altura do retângulo:'))
-var area = base*altura
-window.alert(`Um retângulo com base ${base}cm e altura ${altura}cm possui uma área igual a ${area} cm\u00B2`)
+
+function calcular() {
+    const base = document.getElementById("baseretangulo").value;
+    const altura = document.getElementById("alturaretangulo").value;
+    const area = base*altura
+        alert(`Um retângulo com base ${base}cm e altura ${altura}cm possui uma área igual a ${area} cm\u00B2`);
+    }
+
 
 //Crie uma condicional para verificar se o caractere é uma vogal
 
-let vogal = 'b'; 
+function verificar5() {
+    const vogal = document.getElementById("vogal").value.toLowerCase();
 
-let letra = vogal.toLowerCase();
-
-if (letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u') {
-    console.log(`A letra ${vogal}, é uma vogal.`);
+if (vogal === 'a' || vogal === 'e' || vogal === 'i' || vogal === 'o' || vogal === 'u') {
+    alert(`A letra ${vogal.toUpperCase()}, é uma vogal.`);
 } else {
-    console.log(`A letra ${vogal}, não é uma vogal.`);
+    alert(`A letra ${vogal.toUpperCase()}, não é uma vogal.`);
 }
-
+}
 
 //Crie uma condicional para verificar se o número é par ou é impar
 
 function verificar() {
-    var num = document.getElementById("number").value
-    var number = Number(num)
+    const num = document.getElementById("number").value
+    const number = Number(num)
 
     if(number % 2 == 0) {
         alert(`O número ${number} é par!`)
@@ -115,13 +148,13 @@ function verificar() {
 
 //Crie uma condicional if else if else para verificar se um triângulo é equilátero, isósceles ou escaleno:
 function verificar2() {
-    var ladoA = document.getElementById("lado-a").value;
-    var ladoB = document.getElementById("lado-b").value;
-    var ladoC = document.getElementById("lado-c").value;
+    const ladoA = document.getElementById("lado-a").value;
+    const ladoB = document.getElementById("lado-b").value;
+    const ladoC = document.getElementById("lado-c").value;
     
-    var a = Number(ladoA);
-    var b = Number(ladoB);
-    var c = Number(ladoC);
+    const a = Number(ladoA);
+    const b = Number(ladoB);
+    const c = Number(ladoC);
     
     //&& operador lógico - && retorna verdadeiro caso ambos operando sejam verdadeiros, caso contrário, retorna falso.
     if (a === b && b === c) {
@@ -137,14 +170,14 @@ function verificar2() {
 //Calcule a média do aluno e verifique se ele foi aprovado
 
 function verificar3(){
-    var nota1 = document.getElementById("nota1").value;
-    var nota2 = document.getElementById("nota2").value;
+    const nota1 = document.getElementById("nota1").value;
+    const nota2 = document.getElementById("nota2").value;
 
-    var n1 = Number(nota1)
-    var n2 = Number(nota2)
-    var media = (n1+n2)/2
-    var notaDeCorte=7
-    var notaMinima=5
+    const n1 = Number(nota1)
+    const n2 = Number(nota2)
+    const media = (n1+n2)/2
+    const notaDeCorte=7
+    const notaMinima=5
 
     if (media>=notaDeCorte ){
         alert("O Aluno está aprovado.");
@@ -160,8 +193,9 @@ function verificar3(){
 
 
 // Crie uma variável para armazenar um array com seus hobbies
-var array = ["Viajar", "Assistir filmes e séries", "Ouvir podcasts", "Aproveitar com a fámilia"];
+const array = ["Viajar", " assistir filmes e séries", " ouvir podcasts", " aproveitar com a fámilia", " conhecer restaurantes"];
 console.log(array);
+document.getElementById("hobbies").innerHTML = array;
 
 
 /* 
@@ -171,15 +205,20 @@ Adicione mais duas frutas ao final do array.
 Remova a primeira fruta do array.
 */
 
-var arr = ["Morango", "Uva", "Banana", "Manga", "Abacaxi"];
+const arr = ["Morango", " Uva", " Banana", " Manga", " Abacaxi"];
+document.getElementById("5frutas").innerHTML = arr;
 
 console.log(arr[1]);
+document.getElementById("fruta2").innerHTML = arr[1];
 
-arr[5] = "Pêra"
-arr[6] = "Melância"
+arr[5] = " Pêra"
+arr[6] = " Melância"
 
 console.log(arr)
+document.getElementById("+2frutas").innerHTML = arr;
 
+console.log(arr.shift())
+document.getElementById("-1fruta").innerHTML = arr;
 
 
 /* 
@@ -187,19 +226,28 @@ Crie uma variável e atribua um valor de qualquer tipo (número, string, boolean
 Utilize o operador typeof para verificar o tipo da variável.
 Imprima o tipo da variável no console.
 */
-var verdadeiro = true;
-var falso = false;
-console.log(typeof verdadeiro)
+const verdadeiro = true;
+const falso = false;
+
+        
+function verificaVerdadeiro() {
+alert(`O tipo da variável "verdadeiro" é: ${typeof verdadeiro}`);
+}
+
+        
+function verificaFalso() {
+alert(`O tipo da variável "falso" é: ${typeof falso}`);
+}
 
 /* Crie um programa que simule uma calculadora simples. O usuário deve inserir dois números e um operador (+, -, *, /). Utilize a estrutura switch para realizar a operação correspondente e exibir o resultado. */
 
 function verificar4() {
-    var num1 = document.getElementById("num1").value;
-    var num2 = document.getElementById("num2").value;
-    var operador = document.getElementById("operador").value;
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    const operador = document.getElementById("operador").value;
 
-    var numero1 = Number(num1);
-    var numero2 = Number(num2);
+    const numero1 = Number(num1);
+    const numero2 = Number(num2);
 
     switch (operador) {
         case '+':
